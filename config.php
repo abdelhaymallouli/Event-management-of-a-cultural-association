0c5cd636b1bca440f05149d26a69b101629fcaf1
abdelhaymallouli <abdelhay.mallouli@gmail.com>
2026-01-22 21:40:50 +0100

    .

--- config.php
+++ config.php
@@ -1,12 +1,12 @@
-<?php 
+<?php
 $host = 'localhost';
 $dbname = 'farhaevents';
 $username = 'root';
 $password = '';
 
-try{
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8",$username,$password);
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}catch(PDOException $e){
+} catch (PDOException $e) {
     die('Error: ' . $e->getMessage());
 }
